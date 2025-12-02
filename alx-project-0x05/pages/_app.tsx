1f3d957baa73f6908edc7bd97c22652678529636
enyasystem <enyaelvis@gmail.com>
2025-12-02 17:27:32 +0100

    fix: Correct import path for global styles in _app.tsx

--- alx-project-0x05/pages/_app.tsx
+++ alx-project-0x05/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/layouts/Layout'
-import '@/styles/global.css'
+import '@/styles/globals.css'
 import { CountProvider } from '@/context/CountContext'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
